9b4dd12522eea8cec1cc47da53d51103ab8fa786
ayo <ayoola.adedeji@outlook.com>
2020-09-28 09:47:04 +0100

    made buff delay constant

--- buffsdk/src/main/java/com/buffup/sdk/services/BuffSportApiService.kt
+++ buffsdk/src/main/java/com/buffup/sdk/services/BuffSportApiService.kt
@@ -8,12 +8,13 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.retry
 import javax.inject.Inject
+private const val BUFF_DELAY = 30000L
 
 class BuffSportApiService @Inject constructor(private val endpoints: Endpoints) {
 
     val buffs: Flow<Buff> = flow {
         for (i in 1..5) {
-            delay(30000)
+            delay(BUFF_DELAY)
             endpoints.getBuff(i).body()?.result?.let { buff -> emit(buff) }
         }
     }.retry(2) {e -> throw ServerException(0, e.message) }
